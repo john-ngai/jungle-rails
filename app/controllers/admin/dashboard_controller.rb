bbedc0a54ed3927fde188e711e2e7e4477ef6c9a
mrjohnming <john.ngai@hotmail.com>
2022-03-12 21:13:19 +0000

    Display a live count of the products & categories

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: "Jungle", password: "book"
   
   def show
-  end
+    @total_products = Product.all.count
+    @total_categories = Category.all.count
+  end  
 end
