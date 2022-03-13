be24d04c84fdc2ce97fa0c134041ca41a8f729d6
mrjohnming <john.ngai@hotmail.com>
2022-03-13 01:31:24 +0000

    Define initial methods

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,2 +1,12 @@
 class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: "Jungle", password: "book"
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
 end
