1dc9f54ed24ec1ad46c2b687d789dffc71a73e8e
mrjohnming <john.ngai@hotmail.com>
2022-03-13 01:48:01 +0000

    Update the 'create' method

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -9,5 +9,12 @@ class Admin::CategoriesController < ApplicationController
   end
 
   def create
+    @category = Categories.new(product_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
   end
 end
