e6bda2a83fa89749bb505a4572c85924ec966e34
mrjohnming <john.ngai@hotmail.com>
2022-03-15 23:16:05 +0000

    Create specs

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,6 +2,47 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   
-  describe 'Validation' do
+  describe 'validation' do
+
+    it 'returns a product' do
+      @category = Category.create(name: 'Apparel')
+      @product = @category.products.create(
+        name: 'test name',
+        price: 19.95,
+        quantity: 10
+      )
+      expect(@product).to be_a Product
+      expect(@product).to have_attributes(
+        name: 'test name',
+        price: 19.95,
+        quantity: 10
+      )
+      expect(@product.category).to have_attributes(name: 'Apparel')
+    end 
+    
+    it 'returns the name' do
+      @category = Category.create(name: 'Apparel')
+      @product = @category.products.create(name: 'test name')
+      expect(@product).to have_attributes(name: 'test name')
+    end
+
+    it 'returns the price' do
+      @category = Category.create(name: 'Apparel')
+      @product = @category.products.create(price: 19.95)
+      expect(@product).to have_attributes(price: 19.95)
+    end
+
+    it 'returns the quantity' do
+      @category = Category.create(name: 'Apparel')
+      @product = @category.products.create(quantity: 10)
+      expect(@product).to have_attributes(quantity: 10)
+    end
+
+    it 'returns the category' do
+      @category = Category.create(name: 'Apparel')
+      @product = @category.products.create
+      expect(@product.category).to have_attributes(name: 'Apparel')
+    end
   end
+
 end
