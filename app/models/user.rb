fb62aa678b48fface33bc10f096d42869de78898
mrjohnming <john.ngai@hotmail.com>
2022-03-16 01:39:35 +0000

    Add edge cases to @user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
   has_secure_password
 
   def self.authenticate_with_credentials(email, password)
-    @user = User.find_by(email: email)
+    # Find by email after changing to all characters to lowercase & removing any whitespace.
+    @user = User.find_by(email: email.downcase.strip)
     if @user && @user.authenticate(password)
       @user
     else
