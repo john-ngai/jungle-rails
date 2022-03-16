c25a949189bdcc6d4f45009aa0fca5d44920233b
mrjohnming <john.ngai@hotmail.com>
2022-03-16 01:19:16 +0000

    Define method - authenticate_with_credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  def self.authenticate_with_credentials(email, password)
+    @user = User.find_by(email: email)
+    if @user && @user.authenticate(password)
+      @user
+    else
+      nil
+    end
+  end
 end
